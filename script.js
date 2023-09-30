209ee0120724997a471b609fa880c8fe2a43fa64
Rob Gould <rgould.midwest@gmail.com>
2023-09-30 00:21:52 -0500

    initial commit after repo creation and adding files, comments to .js file, ie. place for pseudo code

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-### passwordGenerator
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
-## My Task
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-This week's Challenge requires you to modify starter code to create an application that enables employees to generate random passwords based on criteria that they’ve selected. This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean and polished, responsive user interface that adapts to multiple screen sizes.
+  passwordText.value = password;
 
-The password can include special characters. If you’re unfamiliar with these, see this [list of password special characters](https://www.owasp.org/index.php/Password_special_characters) from the OWASP Foundation.
+}
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+/* PSEUDO CODE 
+
+
+
+
+
+
+
+/* USER/CCEPTANCE CRITERIA FOR REFERENCE
 
 ## User Story
 
@@ -34,3 +51,6 @@ THEN a password is generated that matches the selected criteria
 WHEN the password is generated
 THEN the password is either displayed in an alert or written to the page
 ```
+
+
+
